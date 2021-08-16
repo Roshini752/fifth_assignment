02f1d95c729af421793a87cd6f2124bbac8419b9
Roshini752 <88433698+Roshini752@users.noreply.github.com>
2021-08-16 16:39:12 +0530

    Update copy_string.c

--- copy_string.c
+++ copy_string.c
@@ -8,18 +8,18 @@ int main()
     int i;
     for(i=0;i<=12;i++)
     {
-        str2[i]=ptr[i];
+        str2[i]=ptr[i]; // by using pointer coping the str to str2
     }
     printf("%s\n",str2);
-    char *ptr2=str2;
+    char *ptr2=str2;  //it will store the base address of string 
     int count=0;
-    while(*ptr2 != '\0')
+    while(*ptr2 != '\0')  // it will check upto null point
     {
-        count++;
+        count++; // to find the length of the string
         ptr2++;
     }
     printf("length of the string %d\n",count);
-    int l=strlen(str2);
+    int l=strlen(str2); // string function to find the length
     printf("%d\n",l);
 }
 
